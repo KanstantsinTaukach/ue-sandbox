85a895d7b0890cb88e24085a1ae94ad616bf8cd6
Kanstantsin Taukach <tovkachkot@gmail.com>
2024-04-17 20:00:15 +0300

    initial commit

---
+++ Source/UESandbox/UESandbox.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UESandbox.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UESandbox, "UESandbox" );
---
+++ Source/UESandbox/UESandboxGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UESandboxGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UESANDBOX_API AUESandboxGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
